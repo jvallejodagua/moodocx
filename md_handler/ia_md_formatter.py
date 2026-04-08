dbdd13edbf4afc1ee52bbc6b3e33c5503f046fb0
Johan Vallejo Dagua <jvallejodagua@gmail.com>
2026-04-07 19:54:19 -0500

    Ajuste a la ventana de contexto

--- md_handler/ia_md_formatter.py
+++ md_handler/ia_md_formatter.py
@@ -9,11 +9,11 @@ class IAMdFormatter:
     def __init__(self, model_path):
         
         self.model_path = model_path / "gemma_2b_it_Q4_K_M.gguf"
-
+        self.max_memory = 8192
         self.ia_model = Llama(
-            model_path=str(self.model_path), 
-            n_ctx=8192, # Memoria de contexto
-            verbose=False)
+            model_path = str(self.model_path), 
+            n_ctx = 8192, # Memoria de contexto
+            verbose = False)
 
     def parse_prompt(self, prompt_input):
         pass
@@ -22,12 +22,12 @@ class IAMdFormatter:
         pass
     
     def example(self):
-        prompt = "Escribe un bucle for en bash."
+        prompt = "Si te especifico un formato de texto objetivo y un texto de entrada. ¿Eres capaz de acomodar el texto de entrada para que sea similar al texto objetivo?"
         print(f"Usuario: {prompt}")
 
         salida = self.ia_model(
             f"<start_of_turn>user\n{prompt}<end_of_turn>\n<start_of_turn>model\n", 
-            max_tokens=256, 
+            max_tokens=int(self.max_memory / 2), 
             stop=["<end_of_turn>"]
         )
 
